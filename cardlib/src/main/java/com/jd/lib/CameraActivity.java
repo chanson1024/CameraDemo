b756960fa6a2cc1577211eada12cde67925ae843
chanson1024 <lovezww2011@gmail.com>
2017-03-14 23:49:22 +0800

    Fix camera

--- cardlib/src/main/java/com/jd/lib/CameraActivity.java
+++ cardlib/src/main/java/com/jd/lib/CameraActivity.java
@@ -1,15 +1,24 @@
 package com.jd.lib;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.hardware.Camera;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
 
 import com.jd.lib.util.DisplayUtil;
 import com.jd.lib.util.ScreenUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -20,6 +29,8 @@ import java.util.List;
 public class CameraActivity extends Activity implements SurfaceHolder.Callback, Camera.PreviewCallback {
 
     public CameraSurfaceView surfaceView;
+    public Button mButton;
+
     public SurfaceHolder surfaceHolder;
 
     public Camera camera;
@@ -33,11 +44,53 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
     }
 
     private void initView() {
+        mButton=(Button)findViewById(R.id.btn_take);
         surfaceView=(CameraSurfaceView)findViewById(R.id.surfaceView);
         surfaceHolder=surfaceView.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture(null,null,jpegCallback);
+            }
+        });
     }
+
+    private Camera.PictureCallback jpegCallback = new Camera.PictureCallback(){
+
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Camera.Parameters ps = camera.getParameters();
+            if(ps.getPictureFormat() == PixelFormat.JPEG){
+                //存储拍照获得的图片
+                String path = savePicture(data);
+                //将图片交给Image程序处理
+//                Uri uri = Uri.fromFile(new File(path));
+//                Intent intent = new Intent();
+//                intent.setAction("android.intent.action.VIEW");
+//                intent.setDataAndType(uri, "image/jpeg");
+//                startActivity(intent);
+            }
+        }
+    };
+
+    private String savePicture(byte[] data) {
+        String imgFileName = "face.jpg";
+        FileOutputStream outputStream;
+        try {
+            File file = new File("/sdcard/" + imgFileName);
+//            if(file.exists())
+//                return null;
+            outputStream = new FileOutputStream(file);
+            outputStream.write(data);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return imgFileName;
+    }
+
 
     @Override
     protected void onDestroy() {
